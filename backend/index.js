2d45ca285c2af900d54e4b1ff90ffc5150f3c88d
hieu <n21dcpt024@student.ptithcm.edu.vn>
2024-05-01 22:38:02 +0700

    base-be

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,34 @@
 const express = require('express')
-const app = express()
+const helmet = require('helmet')
+const morgan = require('morgan')
+const mongoSanitize = require('express-mongo-sanitize')
+const compression = require('compression')
+const fileUpload = require('express-fileupload')
+const cors = require('cors')
 require('dotenv').config()
 
-app.get('/api', (req, res) => {
-  res.send('Hello World!')
+const app = express()
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'))
+}
+app.use(helmet())
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.use(mongoSanitize())
+
+app.use(compression())
+
+app.use(fileUpload({ useTempFiles: true }))
+
+app.use(cors())
+
+app.post('/api', (req, res) => {
+  res.send(req.body)
 })
+
 const PORT = process.env.PORT || 8000
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`)
